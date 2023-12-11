a8b90fbec455428354ef803903145e9c559e637e
Martin Aguero <martin.aguero84@gmail.com>
2023-12-10 22:08:42 -0600

    started working on the getAPI function to fect the data and return it as json

--- script.js
+++ script.js
@@ -8,9 +8,22 @@ var wind = document.getElementById('wind');
 var humidity = document.getElementById('humidity');
 var fiveDayDiv = document.getElementById('fiveday');
 
-//Variable to capture the API URL
-var APIURL = 'api.openweathermap.org';
+//Function to fetch API url data and return it as json
+function getAPI() {
+    //Variable to capture the API URL
+    var apiKey = '70a60e1ab06b68a17e3d748769c9f86a'
+    var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid='+apiKey;
 
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+
+//Function to retrive API data based on user city input
 
 
 
