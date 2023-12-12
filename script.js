401221afdd695f16aa1fca95e962dda4b808602e
Martin Aguero <martin.aguero84@gmail.com>
2023-12-11 20:07:07 -0600

    added to the getAPI function by capturing the city name input and fetching the lat and lon value for it and storing it as a variable.

--- script.js
+++ script.js
@@ -10,21 +10,29 @@ var fiveDayDiv = document.getElementById('fiveday');
 
 //Function to fetch API url data and return it as json
 function getAPI() {
+    //This block of code; including cityValue variable, requestURL,
+    //and fetch function work to get the lat and lon of a city name input.
+    cityValue = cityInput.value;
     //Variable to capture the API URL
     var apiKey = '70a60e1ab06b68a17e3d748769c9f86a'
-    var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid='+apiKey;
+    var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+cityValue+'&limit=1&appid='+apiKey;
 
     fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            console.log(lat);
+            console.log(lon);
+        });
 }
 
-//Function to retrive API data based on user city input
+searchBtn.addEventListener('click', getAPI);
 
+//Function to retrive API data based on user city input
 
 
 //Event listener for the 'search' button
