ff2b842856b5ed3908a5f19e5ad73c378da3e7de
Martin Aguero <martin.aguero84@gmail.com>
2025-01-19 08:05:06 -0600

    fixed layout to be col grid system from bootstrap. pulled in the today date from dayjs for the current weather condition.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,63 +5,81 @@ const divResultsCurrent = document.getElementById('resultsCurrent');
 const divResultsForecast = document.getElementById('resultsForecast');
 const divCurrentTitle = document.getElementById('currentTitle');
 const divForecastTitle = document.getElementById('forecastTitle');
+const btnsRecentSearches = document.getElementById('recentSearches');
 //API Key
 const apiKey = '70a60e1ab06b68a17e3d748769c9f86a';
 //Variable for request URL for city lat & lon
 const requestGeoURL = `https://api.openweathermap.org/data/2.5/weather`;
 //Variable for request URL for weather
 const requestForecastURL = 'https://api.openweathermap.org/data/2.5/forecast';
+
 //Function to create current weather card
 function createCardCurrentWeather(record) {
     //create elements
     const h4El = document.createElement('h4');
-    const divEl = document.createElement('div');
+    const divCol = document.createElement('div');
+    const divCard = document.createElement('div');
     const h5El = document.createElement('h5');
+    const h6ElToday = document.createElement('h6');
+    const divCardBody = document.createElement('div');
     const pElTemp = document.createElement('p');
     const pElTempFeelsLike = document.createElement('p');
     const pElWeatherDesc = document.createElement('p');
     const pElHumidity = document.createElement('p');
     const pElWeatherIcon = document.createElement('p');
     //set attributes
-    divEl.setAttribute('class', 'card ms-4 me-4 my-4');
+    divCol.setAttribute('class', 'col');
+    divCard.setAttribute('class', 'card mx-4 my-4 shadow bg-body-tertiary rounded');
     h5El.setAttribute('class', 'card-header mb-2');
+    h6ElToday.setAttribute('class', 'card-subtitle mt-1');
+    divCardBody.setAttribute('class', 'card-body');
     h4El.setAttribute('class', 'ms-4 mt-4');
     //set text
     h4El.textContent = record.name;
-    h5El.textContent = 'Today';
-    pElTemp.textContent = `Temp: ${Math.trunc(record.main.temp)}`;
-    pElTempFeelsLike.textContent = `Feels Like: ${Math.trunc(record.main.feels_like)}`;
-    pElWeatherDesc.textContent = `Description: ${record.weather[0].description}`;
+    h5El.textContent = 'Now';
+    h6ElToday.textContent = dayjs().format('dddd, MMMM D - h a');
+    pElTemp.textContent = `Temp: ${Math.trunc(record.main.temp)} °`;
+    pElTempFeelsLike.textContent = `Feels Like: ${Math.trunc(record.main.feels_like)} °`;
+    pElWeatherDesc.textContent = `${record.weather[0].description}`;
     pElHumidity.textContent = `Humidity: ${record.main.humidity}`;
     pElWeatherIcon.textContent = record.weather[0].icon;
     //append
-    divEl.append(h5El, pElTemp, pElTempFeelsLike, pElWeatherDesc, pElHumidity, pElWeatherIcon);
+    divCardBody.append(pElTemp, pElTempFeelsLike, pElWeatherDesc, pElHumidity, pElWeatherIcon);
+    h5El.append(h6ElToday);
+    divCard.append(h5El, divCardBody);
+    divCol.append(divCard)
     divCurrentTitle.append(h4El);
-    divResultsCurrent.append(divEl);
+    divResultsCurrent.append(divCol);
 }
 //Function to create cards on webpage
 function createCardsForcast(record) {
     //create elements
-    const divEl = document.createElement('div');
+    const divCol = document.createElement('div');
+    const divCard = document.createElement('div');
     const h5El = document.createElement('h5');
+    const divCardBody = document.createElement('div');
     const pElTemp = document.createElement('p');
     const pElTempFeelsLike = document.createElement('p');
     const pElWeatherDesc = document.createElement('p');
     const pElHumidity = document.createElement('p');
     const pElWeatherIcon = document.createElement('p');
     //set attributes
-    divEl.setAttribute('class', 'card ms-4 me-4 my-4');
+    divCol.setAttribute('class', 'col');
+    divCard.setAttribute('class', 'card mx-4 my-4 shadow bg-body-tertiary rounded');
     h5El.setAttribute('class', 'card-header mb-2');
+    divCardBody.setAttribute('class', 'card-body');
     //set text
-    h5El.textContent = `${dayjs(record.dt_txt).format('dddd - h:mm a')}`;
-    pElTemp.textContent = `Temp: ${Math.trunc(record.main.temp)}`;
-    pElTempFeelsLike.textContent = `Feels Like: ${Math.trunc(record.main.feels_like)}`;
-    pElWeatherDesc.textContent = `Description: ${record.weather[0].description}`;
+    h5El.textContent = `${dayjs(record.dt_txt).format('dddd - h a')}`;
+    pElTemp.textContent = `High/Low: ${Math.trunc(record.main.temp_max)} ° / ${Math.trunc(record.main.temp_min)} °`;
+    pElTempFeelsLike.textContent = `Feel Like: ${Math.trunc(record.main.feels_like)} °`;
+    pElWeatherDesc.textContent = `${record.weather[0].description}`;
     pElHumidity.textContent = `Humidity: ${record.main.humidity}`;
     pElWeatherIcon.textContent = record.weather[0].icon;
     //append
-    divEl.append(h5El, pElTemp, pElTempFeelsLike, pElWeatherDesc, pElHumidity, pElWeatherIcon);
-    divResultsForecast.append(divEl);
+    divCardBody.append(pElTemp, pElTempFeelsLike, pElWeatherDesc, pElHumidity, pElWeatherIcon);
+    divCard.append(h5El, divCardBody);
+    divCol.append(divCard)
+    divResultsForecast.append(divCol);
 }
 //Function to fetch api data
 function getAPI(city) {
@@ -91,7 +109,7 @@ function getAPI(city) {
             //create forecast title element, set attributes and text, and append
             const h4El = document.createElement('h4');
             h4El.setAttribute('class', 'ms-4 mt-4');
-            h4El.textContent = 'Forecast:';
+            h4El.textContent = '5-Day Forecast:';
             divForecastTitle.append(h4El);
             //grab the forecast array from the data
             const arrayListForecast = data.list;
